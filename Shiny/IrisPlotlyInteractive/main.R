8fb1e1d5a123c448e6728a708874358b92c1956f
Jub42 <jube0010@stud.hs-kl.de>
2022-05-31 11:35:54 +0200

    Aufgeraeumt und rconnect hinzugefuegt

--- Shiny/IrisPlotlyInteractive/main.R
+++ Shiny/IrisPlotlyInteractive/main.R
@@ -1,11 +1,5 @@
 source("ui.R")
 source("server.R")
 
-#source("Tablayout/tablayout.R")
-
-#source("Tablayout/flowLayout.R")
-
-#source("Tablayout/hideLayout.R")
-
 # Erstellt eine Shiny App mit Hilfe eines UI/Server-Paares
 shinyApp(uiNavbar, server)
